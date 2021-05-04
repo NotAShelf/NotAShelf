5429c435da5b750db7b792b83889d511bb192b52
NotAShelf <62766066+NotAShelf@users.noreply.github.com>
2021-05-04 21:22:54 +0300

    beginning of the end

---
+++ codes/generate_random_workflow.py
@@ -0,0 +1,17 @@
+import os
+import random
+
+cron_line = '"0 */{prevNo} * * *"'
+
+with open(".github/workflows/rating-chart.yml", "r") as f:
+  wf = f.read()
+  
+randNo = random.randint(1, 8)
+newCron = cron_line.format(prevNo=randNo)
+
+for prevNum in range (1, 9):
+  prevCron = cron_line.format(prevNo=prevNum)
+  if wf.find(prevCron) != -1:
+    wf = wf.replace(prevCron, newCron)
+    break
+print (wf[:-1])
