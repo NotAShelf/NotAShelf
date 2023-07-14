b8ecc8a7e32e9acce1d5f59b29b5a713af57ffef
NotAShelf <raf@notashelf.dev>
2023-07-14 22:16:41 +0300

    update code stats generate script

--- codes/generate_images.py
+++ codes/generate_images.py
@@ -1,19 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
+
 
 import asyncio
 import os
-import re
-
 import aiohttp
 
+
 from github_stats import Stats
+
 
 def generate_output_folder() -> None:
     """
     Create the output folder if it does not already exist
     """
-    if not os.path.isdir("generated"):
-        os.mkdir("generated")
+    try:
+        os.makedirs("generated", exist_ok=True)
+    except OSError as e:
+        print(f"Error creating output folder: {str(e)}")
+
 
 async def generate_overview(s: Stats) -> None:
     """
@@ -23,15 +27,14 @@ async def generate_overview(s: Stats) -> None:
     with open("templates/overview.svg", "r") as f:
         output = f.read()
 
-    output = re.sub("{{ name }}", await s.name, output)
-    output = re.sub("{{ stars }}", f"{await s.stargazers:,}", output)
-    output = re.sub("{{ forks }}", f"{await s.forks:,}", output)
-    output = re.sub("{{ contributions }}", f"{await s.total_contributions:,}",
-                    output)
+    output = output.replace("{{ name }}", await s.name)
+    output = output.replace("{{ stars }}", f"{await s.stargazers:,}")
+    output = output.replace("{{ forks }}", f"{await s.forks:,}")
+    output = output.replace("{{ contributions }}", f"{await s.total_contributions:,}")
     changed = (await s.lines_changed)[0] + (await s.lines_changed)[1]
-    output = re.sub("{{ lines_changed }}", f"{changed:,}", output)
-    output = re.sub("{{ views }}", f"{await s.views:,}", output)
-    output = re.sub("{{ repos }}", f"{len(await s.all_repos):,}", output)
+    output = output.replace("{{ lines_changed }}", f"{changed:,}")
+    output = output.replace("{{ views }}", f"{await s.views:,}")
+    output = output.replace("{{ repos }}", f"{len(await s.all_repos):,}")
 
     generate_output_folder()
     with open("generated/overview.svg", "w") as f:
@@ -48,21 +51,21 @@ async def generate_languages(s: Stats) -> None:
 
     progress = ""
     lang_list = ""
-    sorted_languages = sorted((await s.languages).items(), reverse=True,
-                              key=lambda t: t[1].get("size"))
+    sorted_languages = sorted(
+        (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size")
+    )
     delay_between = 150
     for i, (lang, data) in enumerate(sorted_languages):
-        color = data.get("color")
-        color = color if color is not None else "#000000"
-        ratio = [.98, .02]
-        if data.get("prop", 0) > 50:
-            ratio = [.99, .01]
+        color = data.get("color", "#000000")
+        ratio = [0.98, 0.02] if data.get("prop", 0) > 50 else [0.99, 0.01]
         if i == len(sorted_languages) - 1:
             ratio = [1, 0]
-        progress += (f'<span style="background-color: {color};'
-                     f'width: {(ratio[0] * data.get("prop", 0)):0.3f}%;'
-                     f'margin-right: {(ratio[1] * data.get("prop", 0)):0.3f}%;" '
-                     f'class="progress-item"></span>')
+        progress += (
+            f'<span style="background-color: {color};'
+            f'width: {(ratio[0] * data.get("prop", 0)):0.3f}%;'
+            f'margin-right: {(ratio[1] * data.get("prop", 0)):0.3f}%;" '
+            f'class="progress-item"></span>'
+        )
         lang_list += f"""
 <li style="animation-delay: {i * delay_between}ms;">
 <svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:{color};"
@@ -71,20 +74,15 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">{lang}</span>
 <span class="percent">{data.get("prop", 0):0.2f}%</span>
 </li>
-
 """
 
-    output = re.sub(r"{{ progress }}", progress, output)
-    output = re.sub(r"{{ lang_list }}", lang_list, output)
+    output = output.replace(r"{{ progress }}", progress)
+    output = output.replace(r"{{ lang_list }}", lang_list)
 
     generate_output_folder()
     with open("generated/languages.svg", "w") as f:
         f.write(output)
 
-
-################################################################################
-# Main Function
-################################################################################
 
 async def main() -> None:
     """
@@ -92,22 +90,29 @@ async def main() -> None:
     """
     access_token = os.getenv("ACCESS_TOKEN")
     if not access_token:
-        # access_token = os.getenv("GITHUB_TOKEN")
         raise Exception("A personal access token is required to proceed!")
     user = os.getenv("GITHUB_ACTOR")
     exclude_repos = os.getenv("EXCLUDED")
-    exclude_repos = ({x.strip() for x in exclude_repos.split(",")}
-                     if exclude_repos else None)
+    exclude_repos = (
+        {x.strip() for x in exclude_repos.split(",")} if exclude_repos else None
+    )
     exclude_langs = os.getenv("EXCLUDED_LANGS")
-    exclude_langs = ({x.strip() for x in exclude_langs.split(",")}
-                     if exclude_langs else None)
-    consider_forked_repos = len(os.getenv("COUNT_STATS_FROM_FORKS")) != 0
+    exclude_langs = (
+        {x.strip() for x in exclude_langs.split(",")} if exclude_langs else None
+    )
+    consider_forked_repos = bool(os.getenv("COUNT_STATS_FROM_FORKS"))
+
     async with aiohttp.ClientSession() as session:
-        s = Stats(user, access_token, session, exclude_repos=exclude_repos,
-                  exclude_langs=exclude_langs,
-                  consider_forked_repos=consider_forked_repos)
+        s = Stats(
+            user,
+            access_token,
+            session,
+            exclude_repos=exclude_repos,
+            exclude_langs=exclude_langs,
+            consider_forked_repos=consider_forked_repos,
+        )
         await asyncio.gather(generate_languages(s), generate_overview(s))
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
