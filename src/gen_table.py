4f83d1e58b60b083d53805791f642053fb9737cc
NotAShelf <raf@notashelf.dev>
2023-12-31 07:59:57 +0300

    even more style

--- src/gen_table.py
+++ src/gen_table.py
@@ -6,17 +6,29 @@ def generate_markdown_table(json_file):
     with open(json_file, "r") as file:
         data = json.load(file)
 
-    table = ""
+    nix_projects = []
+    bash_go_projects = []
 
     for category, items in data.items():
-        table += f"| **{category}** |\n"
-        table += "| --- |\n"
+        if category == "Nix":
+            for item in items:
+                nix_projects.append(
+                    f"[{item['title']}]({item['link']}) - {item['description']}"
+                )
+        elif category == "Bash, Go, Python, Typescript, Java, JS":
+            for item in items:
+                bash_go_projects.append(
+                    f"[{item['title']}]({item['link']}) - {item['description']}"
+                )
 
-        projects = ""
-        for item in items:
-            projects += f"[{item['title']}]({item['link']}) - {item['description']}<br>"
+    table = "| **Nix** | **Bash, Go, Python, Typescript, Java, JS** |\n"
+    table += "| --- | --- |\n"
 
-        table += f"| • {projects} |\n\n"
+    max_len = max(len(nix_projects), len(bash_go_projects))
+    for i in range(max_len):
+        nix_item = f"• {nix_projects[i]}" if i < len(nix_projects) else ""
+        bash_go_item = f"• {bash_go_projects[i]}" if i < len(bash_go_projects) else ""
+        table += f"| {nix_item} | {bash_go_item} |\n"
 
     return table
 
