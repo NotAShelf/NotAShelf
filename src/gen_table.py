4b3dac4ffd01aa1e8deb53ef141bd9b6026da661
NotAShelf <raf@notashelf.dev>
2024-10-23 08:37:55 +0300

    streamline `gen_table.py`
    Consistency, reduced duplication and error handling. Neat!

--- src/gen_table.py
+++ src/gen_table.py
@@ -1,32 +1,38 @@
 import json
 import argparse
+import os
 
 
 def generate_markdown_table(json_file):
+    # check if file exists
+    if not os.path.exists(json_file):
+        raise FileNotFoundError(f"The file {json_file} does not exist.")
+
     with open(json_file, "r") as file:
         data = json.load(file)
 
-    nix_projects = []
-    other_projects = []
+    project_categories = {"Nix": [], "Go, Rust, Python, JavaScript, TypeScript": []}
 
+    # iterate through each category and project in the json data
     for category, items in data.items():
-        if category == "Nix":
-            for item in items:
-                nix_projects.append(
-                    f"• [{item['title']}]({item['link']}) - {item['description']}"
-                )
-        elif category == "Bash, Go, Python, Typescript, Java, JS":
-            for item in items:
-                other_projects.append(
-                    f"• [{item['title']}]({item['link']}) - {item['description']}"
-                )
+        if category in project_categories:
+            project_categories[category].extend(
+                f"• [{item['title']}]({item['link']}) - {item['description']}"
+                for item in items
+            )
 
-    nix_content = "<br>".join(nix_projects)
-    bash_go_content = "<br>".join(other_projects)
+    # convert project lists to <br> separated strings
+    nix_content = "<br>".join(project_categories["Nix"])
+    other_content = "<br>".join(
+        project_categories["Go, Rust, Python, JavaScript, TypeScript"]
+    )
 
-    table = "| **Nix** | **Go, Rust, Python, JavaScript, TypeScript** |\n"
-    table += "| --- | --- |\n"
-    table += f"| {nix_content} | {bash_go_content} |\n"
+    # generate markdown table
+    table = (
+        "| **Nix** | **Go, Rust, Python, JavaScript, TypeScript** |\n"
+        "| --- | --- |\n"
+        f"| {nix_content} | {other_content} |\n"
+    )
 
     return table
 
@@ -36,9 +42,12 @@ if __name__ == "__main__":
         description="Generate a Markdown table from a JSON file."
     )
     parser.add_argument("file_path", type=str, help="Path to the JSON file")
-
     args = parser.parse_args()
+
     json_file_path = args.file_path
 
-    markdown_table = generate_markdown_table(json_file_path)
-    print(markdown_table)
+    try:
+        markdown_table = generate_markdown_table(json_file_path)
+        print(markdown_table)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error: {e}")
